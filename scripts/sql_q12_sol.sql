60dd9c8ffceb6f9e641af4b8d64877886ce9a7c4
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-06-26 08:39:13 -0700

    Update sql_q12_sol.sql

--- scripts/sql_q12_sol.sql
+++ scripts/sql_q12_sol.sql
@@ -1,1 +1,12 @@
 -- 12. Write a SQL query that returns the total number of goals scored by each position on each country’s team. Do not include positions which scored no goals.
+
+WITH cte AS
+(SELECT goal_id, g.team_id, g.player_id, pm.posi_to_play, s.country_name
+FROM goal_details AS g
+INNER JOIN soccer_country AS s ON team_id = country_id
+INNER JOIN player_mast AS pm ON g.player_id = pm.player_id)
+SELECT 
+	DISTINCT COUNT(posi_to_play) OVER(PARTITION BY country_name, posi_to_play) AS goals_per_pos,
+    country_name,
+    posi_to_play
+FROM cte
