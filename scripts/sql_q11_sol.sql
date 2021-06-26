74ded39b0b11af8f82782d3dd4bc3621a5059771
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-06-26 07:59:26 -0700

    script 11 and 12

---
+++ scripts/sql_q11_sol.sql
@@ -0,0 +1,5 @@
+-- 11. Write a SQL query to find the players, their jersey number, and playing club who were the goalkeepers for England in EURO Cup 2016.
+
+SELECT player_name, jersey_no, playing_club
+FROM player_mast
+WHERE team_id = 1206 AND posi_to_play = 'GK'
---
+++ scripts/sql_q12_sol.sql
@@ -0,0 +1,1 @@
+-- 12. Write a SQL query that returns the total number of goals scored by each position on each country’s team. Do not include positions which scored no goals.
