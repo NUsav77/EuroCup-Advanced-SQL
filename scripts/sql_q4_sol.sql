fd25e1af1cbaf56dd8b8307d1748d9a38e884687
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-07-06 18:51:09 -0700

    Update sql_q4_sol.sql

--- scripts/sql_q4_sol.sql
+++ scripts/sql_q4_sol.sql
@@ -7,5 +7,6 @@ SELECT CASE WHEN m.play_stage = 'G' THEN '1 - Group'
 		COUNT(in_out) AS num_substitutions
 FROM player_in_out
 INNER JOIN match_mast AS m
+USING(match_no)
 GROUP BY play_stage 
 ORDER BY stage
